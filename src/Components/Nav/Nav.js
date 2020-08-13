b0414a4c8417e313b3ec7d2438032f748e57ab79
cris93g <cristhianswork@gmail.com>
2020-08-13 01:55:15 -0500

    adding links

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Nav.css'
+import {Link} from 'react-router-dom'
 const Nav = ()=>{
     return (
       <div className='nav-container'>
         <div className='left-nav'>Cristian</div>
         <div className='right-nav'>
-          <div>Projects</div>
-          <div>About</div>
-          <div>Contact</div>
+          <Link to='/projects'> <div>Projects</div></Link>
+          <Link to='/about'>
+          <div>About</div></Link>
+          <Link to='/contact'>
+          <div>Contact</div></Link>
         </div>
       </div>
     );
